4b803ed1ed3ab75441db986ba4a3d0a730ab3bf3
NigelFerrefe <nferrefe@gmail.com>
2024-12-18 15:50:59 +0100

    deploy building page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import './App.css'
 
 /*Pages*/
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProjectsPage from "./pages/ProjectsPage";
 import ArtPage from "./pages/ArtPage";
@@ -14,12 +14,12 @@ function App() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<ErrorPage/>} />
         
-        <Route path="/" element={<HomePage/>} />
+        {/* <Route path="/" element={<HomePage/>} /> */}
         <Route path="/about" element={<AboutPage/>} />
         <Route path="/projects" element={<ProjectsPage/>} />
         <Route path="/art" element={<ArtPage/>} />
-        <Route path="*" element={<ErrorPage/>} />
       </Routes>
     </div>
   )
